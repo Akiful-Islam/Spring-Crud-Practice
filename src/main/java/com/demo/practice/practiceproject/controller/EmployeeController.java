21c58277de37ce149cb52c865eef60c762a756eb
Arian Islam <arian.islam024@gmail.com>
2023-04-11 11:42:10 +0600

    fix: Fixed local database connection

--- src/main/java/com/demo/practice/practiceproject/controller/EmployeeController.java
+++ src/main/java/com/demo/practice/practiceproject/controller/EmployeeController.java
@@ -1,13 +1,14 @@
 package com.demo.practice.practiceproject.controller;
 
-import com.demo.practice.practiceproject.dao.EmployeeDAO;
 import com.demo.practice.practiceproject.entity.Employee;
 import com.demo.practice.practiceproject.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 @RequestMapping("/api")
 public class EmployeeController {
     private EmployeeService employeeService;

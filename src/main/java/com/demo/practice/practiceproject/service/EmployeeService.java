2f719bc437a9e5de6b40fa85b683e4ab8ab932bf
Arian Islam <arian.islam024@gmail.com>
2023-04-11 17:24:56 +0600

    change: lombok implemented, major refactors

--- src/main/java/com/demo/practice/practiceproject/service/EmployeeService.java
+++ src/main/java/com/demo/practice/practiceproject/service/EmployeeService.java
@@ -1,13 +1,40 @@
 package com.demo.practice.practiceproject.service;
 
+import com.demo.practice.practiceproject.dao.EmployeeRepository;
 import com.demo.practice.practiceproject.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface EmployeeService {
+@Service
+public class EmployeeService {
 
-    List<Employee> findAll();
-    Employee findById(int id);
-    Employee save(Employee employee);
-    void deleteById(int id);
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee findById(int id) {
+        Optional<Employee> result = employeeRepository.findById(id);
+        if (result.isEmpty()) {
+            throw new RuntimeException("Employee not found with id: " + id);
+        }
+        return result.get();
+    }
+
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteById(int id) {
+        employeeRepository.deleteById(id);
+    }
 }

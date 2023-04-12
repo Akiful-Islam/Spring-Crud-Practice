5e199dc6e63fa1d4d01add195792a5a855a444cf
Arian Islam <arian.islam024@gmail.com>
2023-04-12 13:27:09 +0600

    Revert "feat(spring rest api): Imported spring rest api and deleted self made employee controller and services"
    This reverts commit 42f01a25fe71263a539f341905184361a3777af2.

---
+++ src/main/java/com/demo/practice/practiceproject/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.demo.practice.practiceproject.service;
+
+import com.demo.practice.practiceproject.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+    Employee findById(int id);
+    Employee save(Employee employee);
+    void deleteById(int id);
+}
---
+++ src/main/java/com/demo/practice/practiceproject/service/EmployeeServiceImpl.java
@@ -0,0 +1,44 @@
+package com.demo.practice.practiceproject.service;
+
+import com.demo.practice.practiceproject.dao.EmployeeRepository;
+import com.demo.practice.practiceproject.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        Optional<Employee> result = employeeRepository.findById(id);
+        if (result.isEmpty()) {
+            throw new RuntimeException("Employee not found with id: " + id);
+        }
+        return result.get();
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        employeeRepository.deleteById(id);
+    }
+}

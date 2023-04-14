0e648a595752464f1017e55751b25d79572bad57
Arian <arian.islam024@gmail.com>
2023-04-14 12:14:43 +0600

    feat: auto create db and table

--- src/main/java/com/demo/practice/practiceproject/entity/Employee.java
+++ src/main/java/com/demo/practice/practiceproject/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.demo.practice.practiceproject.entity;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -12,5 +13,4 @@ public class Employee {
     @NonNull private String firstName;
     @NonNull private String lastName;
     @NonNull private String email;
-
 }

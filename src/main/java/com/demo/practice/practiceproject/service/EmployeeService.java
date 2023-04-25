1fdccd5dfa4fabad24a98a1ade4a3de4bc94793e
Arian <arian.islam024@gmail.com>
2023-04-25 11:28:03 +0600

    change(jpa): removed DAO and implemented JPA repository

--- src/main/java/com/demo/practice/practiceproject/service/EmployeeService.java
+++ src/main/java/com/demo/practice/practiceproject/service/EmployeeService.java
@@ -1,22 +1,23 @@
 package com.demo.practice.practiceproject.service;
 
-import com.demo.practice.practiceproject.dao.EmployeeDAO;
 import com.demo.practice.practiceproject.entity.Employee;
+import com.demo.practice.practiceproject.repository.EmployeeRepository;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class EmployeeService {
 
-    private EmployeeDAO employeeDAO;
+    private final EmployeeRepository employeeRepository;
 
-    public EmployeeService(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @PostConstruct
@@ -24,18 +25,30 @@ public class EmployeeService {
         System.out.println("Employee Service created");
     }
 
-    public List<Employee> findAll(int page, int size, String sortBy,String sortOrder) {return employeeDAO.findAll(page,size,sortBy,sortOrder);}
+    public List<Employee> findAll(int page, int size, String sortBy, String sortOrder) {
+        Sort sort = sortOrder.equals("asc") ? Sort.by(Sort.Direction.ASC, sortBy) : Sort.by(Sort.Direction.DESC, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return employeeRepository.findAll(pageable).getContent();
+    }
 
-    public Employee findById(int id) {
-        return employeeDAO.findById(id);
+    public Employee findById(long id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isEmpty()) {
+            throw new RuntimeException("Employee not found");
+        }
+        return employee.get();
     }
 
     public Employee save(Employee employee) {
-        return employeeDAO.save(employee);
+        return employeeRepository.save(employee);
     }
 
-    public void deleteById(int id) {
-        employeeDAO.deleteById(id);
+    public void deleteById(long id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isEmpty()) {
+            throw new RuntimeException("Employee not found");
+        }
+        employeeRepository.deleteById(id);
     }
 
     @PreDestroy

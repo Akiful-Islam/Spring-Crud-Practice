0d3d7d2cf6f9c42c821c412d41df5939861c3d4c
Arian <arian.islam024@gmail.com>
2023-05-03 13:36:00 +0600

    refactor: minor changes

--- src/main/java/com/demo/practice/practiceproject/repository/EmployeeRepository.java
+++ src/main/java/com/demo/practice/practiceproject/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.demo.practice.practiceproject.repository;
 
 import com.demo.practice.practiceproject.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }

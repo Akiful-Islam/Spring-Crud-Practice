e6347716d225696ce7e62bed0474eb15cb687ad7
Arian <arian.islam024@gmail.com>
2023-05-18 12:04:32 +0600

    change: search methods now Return page

--- src/main/java/com/demo/practice/practiceproject/repository/EmployeeRepository.java
+++ src/main/java/com/demo/practice/practiceproject/repository/EmployeeRepository.java
@@ -1,15 +1,15 @@
 package com.demo.practice.practiceproject.repository;
 
 import com.demo.practice.practiceproject.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     boolean existsByEmail(String email);
     boolean existsByPhoneNumber(String phoneNumber);
-    List<Employee> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase (String firstName, String lastName);
-    List<Employee> findByPhoneNumberContaining( String phoneNumber);
+    Page<Employee> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase (String firstName, String lastName, Pageable pageable);
+    Page<Employee> findByPhoneNumberContaining( String phoneNumber, Pageable pageable);
 }

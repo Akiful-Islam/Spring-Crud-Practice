db2839ce9c44827fc01f3be5da2b74ab8a0507a6
Arian <arian.islam024@gmail.com>
2023-05-18 11:44:49 +0600

    feat: updated repository to find by name and phone number

--- src/main/java/com/demo/practice/practiceproject/repository/EmployeeRepository.java
+++ src/main/java/com/demo/practice/practiceproject/repository/EmployeeRepository.java
@@ -4,8 +4,12 @@ import com.demo.practice.practiceproject.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     boolean existsByEmail(String email);
     boolean existsByPhoneNumber(String phoneNumber);
+    List<Employee> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase (String firstName, String lastName);
+    List<Employee> findByPhoneNumberContaining( String phoneNumber);
 }
